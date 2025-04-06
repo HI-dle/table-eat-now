06c27f45d4b8c939fcd4c8714c01d1f99eaaeeda
hyezuu <duudu0704@gmail.com>
2025-04-07 06:07:47 +0900

    [#30][review] refactor: component scan 제거

--- review/src/main/java/table/eat/now/review/ReviewApplication.java
+++ review/src/main/java/table/eat/now/review/ReviewApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"table.eat.now.review", "table.eat.now.common"})
 public class ReviewApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
