d79fc82ae6a0d3f78fc48325f0d481e84b4374ad
je-pa <jieun0502@gmail.com>
2025-04-07 06:14:13 +0900

    [#31][common] AutoConfig 추가

---
+++ common/src/main/java/table/eat/now/common/config/AutoConfig.java
@@ -0,0 +1,10 @@
+package table.eat.now.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "table.eat.now.common")
+public class AutoConfig {
+
+}
