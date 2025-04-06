8d8e7456dd90496be331e3c048d4bd629056e966
hyezuu <duudu0704@gmail.com>
2025-04-07 06:07:52 +0900

    [#30][payment] refactor: component scan 제거

--- payment/src/main/java/table/eat/now/payment/PaymentApplication.java
+++ payment/src/main/java/table/eat/now/payment/PaymentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"table.eat.now.payment", "table.eat.now.common"})
 public class PaymentApplication {
 
 	public static void main(String[] args) {
