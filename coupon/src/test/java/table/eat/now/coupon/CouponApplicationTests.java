e7ab897a19c614232ceb525533bd004bc154ca7e
HanaHww2 <hanahww2@gmail.com>
2025-04-07 12:39:02 +0900

    [#11][coupon] 쿠폰 모듈 세팅

---
+++ coupon/src/test/java/table/eat/now/coupon/CouponApplicationTests.java
@@ -0,0 +1,14 @@
+package table.eat.now.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
