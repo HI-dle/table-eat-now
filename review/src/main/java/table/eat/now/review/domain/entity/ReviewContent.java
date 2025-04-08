bbab90aed453aff272819772b412b15e4286b1d8
hyezuu <duudu0704@gmail.com>
2025-04-09 02:33:01 +0900

    [#45][review] feat: 리뷰 컨텐츠 생성메서드

--- review/src/main/java/table/eat/now/review/domain/entity/ReviewContent.java
+++ review/src/main/java/table/eat/now/review/domain/entity/ReviewContent.java
@@ -2,14 +2,47 @@ package table.eat.now.review.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
 
+@Getter
 @Embeddable
 public class ReviewContent {
 	@Column(name = "content", nullable = false)
 	private String content;
 
-	@Column(name = "rating", nullable = false)
+	@Column(name = "rating", nullable = false, columnDefinition = "TEXT")
 	private Integer rating;
+
+	public static ReviewContent create(String content, Integer rating) {
+		validateNull(content, rating);
+		validateContent(content);
+		validateRating(rating);
+
+		return new ReviewContent(content, rating);
+	}
+
+	private static void validateNull(String content, Integer rating) {
+		if (content == null || rating == null) {
+			throw new IllegalArgumentException("내용과 평점은 null이 될 수 없습니다.");
+		}
+	}
+
+	private static void validateContent(String content) {
+		if (content.isBlank()) {
+			throw new IllegalArgumentException("내용은 비어있을 수 없습니다.");
+		}
+	}
+
+	private static void validateRating(Integer rating) {
+		if (rating < 0 || rating > 5) {
+			throw new IllegalArgumentException("평점은 0에서 5 사이의 값이어야 합니다.");
+		}
+	}
+
+	private ReviewContent(String content, Integer rating) {
+		this.content = content;
+		this.rating = rating;
+	}
 
 	protected ReviewContent() {
 	}
