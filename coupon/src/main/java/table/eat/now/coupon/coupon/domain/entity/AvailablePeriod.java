78765a964174fb447b1c4a3c8e04b0bd185d9c7c
HanaHww2 <hanahww2@gmail.com>
2025-04-08 13:08:18 +0900

    [#39][coupon] 쿠폰 생성 기능 코드 보완

--- coupon/src/main/java/table/eat/now/coupon/coupon/domain/entity/AvailablePeriod.java
+++ coupon/src/main/java/table/eat/now/coupon/coupon/domain/entity/AvailablePeriod.java
@@ -18,13 +18,21 @@ public class AvailablePeriod {
 
   public AvailablePeriod(LocalDateTime startAt, LocalDateTime endAt) {
 
+    validatePeriod(startAt, endAt);
+
+    this.startAt = startAt;
+    this.endAt = endAt;
+  }
+
+  private void validatePeriod(LocalDateTime startAt, LocalDateTime endAt) {
     if (startAt == null || endAt == null) {
       throw new IllegalArgumentException("기간 정보는 필수입니다.");
+    }
+    if (!LocalDateTime.now().isBefore(startAt)) {
+      throw new IllegalArgumentException("시작일이 현재보다 이전일 수 없습니다.");
     }
     if (!startAt.isBefore(endAt)) {
       throw new IllegalArgumentException("시작일이 종료일보다 나중일 수 없습니다.");
     }
-    this.startAt = startAt;
-    this.endAt = endAt;
   }
 }
