2208422b163197f3d2f9cbff8e0f8b943f1e8da7
HanaHww2 <hanahww2@gmail.com>
2025-04-08 18:48:51 +0900

    [#47][coupon] 도메인 코드 보완
    - pg에서 uuid / varchar 간 타입매핑 오류로 인해 UUID 필드를 String 으로 변경

--- coupon/src/main/java/table/eat/now/coupon/coupon/domain/entity/CouponType.java
+++ coupon/src/main/java/table/eat/now/coupon/coupon/domain/entity/CouponType.java
@@ -1,8 +1,9 @@
 package table.eat.now.coupon.coupon.domain.entity;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum CouponType {
   FIXED_DISCOUNT("정액 할인"),
   PERCENT_DISCOUNT("정률 할인"),
