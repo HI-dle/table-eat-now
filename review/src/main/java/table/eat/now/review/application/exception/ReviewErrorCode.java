3dd73778e66e54ae89481814dac339abe4381df0
hyezuu <duudu0704@gmail.com>
2025-04-10 11:01:56 +0900

    [#77][review] feat: ReviewErrorCode 추가

--- review/src/main/java/table/eat/now/review/application/exception/ReviewErrorCode.java
+++ review/src/main/java/table/eat/now/review/application/exception/ReviewErrorCode.java
@@ -9,7 +9,8 @@ public enum ReviewErrorCode implements ErrorCode {
 
 	REVIEW_NOT_FOUND("해당 리뷰를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 	SERVICE_USER_MISMATCH("서비스 정보와 사용자 정보가 일치하지 않습니다.", HttpStatus.FORBIDDEN),
-	REVIEW_IS_INVISIBLE("비공개 처리된 리뷰입니다", HttpStatus.FORBIDDEN),
+	REVIEW_IS_INVISIBLE("비공개 처리된 리뷰입니다.", HttpStatus.FORBIDDEN),
+	MODIFY_PERMISSION_DENIED("수정 요청에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN),
 	;
 
 	private final String message;
