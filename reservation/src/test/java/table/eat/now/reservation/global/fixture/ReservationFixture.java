c3ff3e84860d7582cfc0c274d751a3b9f2187f6b
je-pa <jieun0502@gmail.com>
2025-04-12 04:07:11 +0900

    [#74][reservation] test: 예약 생성

---
+++ reservation/src/test/java/table/eat/now/reservation/global/fixture/ReservationFixture.java
@@ -0,0 +1,15 @@
+/**
+ * @author : jieun(je-pa)
+ * @Date : 2025. 04. 11.
+ */
+package table.eat.now.reservation.global.fixture;
+
+
+public class ReservationFixture {
+
+//  public static Reservation createRestaurant(
+//      Long id,
+//  ) {
+//
+//  }
+}
