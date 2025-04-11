221ce6a45d00278a64358344abe026a37cf89b01
HanaHww2 <hanahww2@gmail.com>
2025-04-12 01:30:56 +0900

    [#96][coupon] 사용자 쿠폰 선점 해제 스케쥴링 추가

---
+++ coupon/src/main/java/table/eat/now/coupon/coupon/application/scheduler/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package table.eat.now.coupon.coupon.application.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
