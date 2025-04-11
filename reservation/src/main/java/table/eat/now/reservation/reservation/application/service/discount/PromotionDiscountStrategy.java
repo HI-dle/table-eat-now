0f9a3675dcaf3d8f3bd2f652cebef83e7ee32be9
je-pa <jieun0502@gmail.com>
2025-04-11 18:26:31 +0900

    [#74][reservation] refactor: 프로모션 할인 전략 패턴

--- reservation/src/main/java/table/eat/now/reservation/reservation/application/service/discount/PromotionDiscountStrategy.java
+++ reservation/src/main/java/table/eat/now/reservation/reservation/application/service/discount/PromotionDiscountStrategy.java
@@ -5,8 +5,10 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import table.eat.now.common.exception.CustomException;
 import table.eat.now.reservation.reservation.application.client.dto.response.GetPromotionsInfo;
 import table.eat.now.reservation.reservation.application.client.dto.response.GetPromotionsInfo.Promotion;
+import table.eat.now.reservation.reservation.application.exception.ReservationErrorCode;
 import table.eat.now.reservation.reservation.application.service.dto.request.CreateReservationCommand.PaymentDetail;
 
 @RequiredArgsConstructor
@@ -17,22 +19,22 @@ public class PromotionDiscountStrategy implements DiscountStrategy {
   @Override
   public void validate(BigDecimal totalPrice, PaymentDetail paymentDetail, LocalDateTime reservationDate) {
     var promotion = Optional.ofNullable(promotions.get(paymentDetail.detailReferenceId()))
-        .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 프로모션입니다."));
+        .orElseThrow(() -> CustomException.from(ReservationErrorCode.PROMOTION_NOT_FOUND));
 
     // 1. 예약 날짜 유효성
     if (reservationDate.isBefore(promotion.startTime()) || reservationDate.isAfter(promotion.endTime())) {
-      throw new IllegalArgumentException("프로모션 적용 기간이 아닙니다.");
+      throw CustomException.from(ReservationErrorCode.PROMOTION_INVALID_PERIOD);
     }
 
     // 2. 상태 확인
     if (promotion.promotionStatus() != GetPromotionsInfo.Promotion.PromotionStatus.RUNNING) {
-      throw new IllegalArgumentException("진행중인 프로모션이 아닙니다.");
+      throw CustomException.from(ReservationErrorCode.PROMOTION_INVALID_RUNNING);
     }
 
     // 3. 할인 금액 확인
     BigDecimal expected = BigDecimal.valueOf(promotion.discountPrice());
     if (expected.compareTo(paymentDetail.amount()) != 0) {
-      throw new IllegalArgumentException("프로모션 할인 금액이 일치하지 않습니다.");
+      throw CustomException.from(ReservationErrorCode.INVALID_PROMOTION_DISCOUNT);
     }
   }
 }
