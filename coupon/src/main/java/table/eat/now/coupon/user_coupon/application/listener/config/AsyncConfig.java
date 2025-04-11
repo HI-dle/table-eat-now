4fe3ba99af07d3dcf0e76caea6f19f9f93c2aa32
HanaHww2 <hanahww2@gmail.com>
2025-04-12 04:33:49 +0900

    [#101][coupon] feat: asyncExecutor 빈 수정

--- coupon/src/main/java/table/eat/now/coupon/user_coupon/application/listener/config/AsyncConfig.java
+++ coupon/src/main/java/table/eat/now/coupon/user_coupon/application/listener/config/AsyncConfig.java
@@ -3,6 +3,7 @@ package table.eat.now.coupon.user_coupon.application.listener.config;
 import java.util.concurrent.Executor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,13 +16,13 @@ public class AsyncConfig implements AsyncConfigurer {
 
   private final AsyncUncaughtExceptionHandler asyncUncaughtExceptionHandler;
 
-  @Override
-  public Executor getAsyncExecutor() {
+  @Bean("async-listener")
+  public Executor asyncListenerExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     executor.setCorePoolSize(3);
     executor.setMaxPoolSize(30);
     executor.setQueueCapacity(100);
-    executor.setThreadNamePrefix("AsyncThread-");
+    executor.setThreadNamePrefix("AsyncListener-");
     executor.initialize();
     return executor;
   }
