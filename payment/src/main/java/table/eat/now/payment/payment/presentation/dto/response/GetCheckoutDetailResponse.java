178f2881dba0b02693025e7612f97e6063e62d60
hyezuu <duudu0704@gmail.com>
2025-04-13 12:18:38 +0900

    [#103][payment] feat : 결제 관련 http 테스트 및 수정

--- payment/src/main/java/table/eat/now/payment/payment/presentation/dto/response/GetCheckoutDetailResponse.java
+++ payment/src/main/java/table/eat/now/payment/payment/presentation/dto/response/GetCheckoutDetailResponse.java
@@ -1,9 +1,11 @@
 package table.eat.now.payment.payment.presentation.dto.response;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record GetCheckoutDetailResponse(
-    UUID idempotencyKey, Long customerId, BigDecimal amount, String orderName) {
+    String idempotencyKey,
+    String customerKey,
+    BigDecimal amount,
+    String orderName) {
 
 }
