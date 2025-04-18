f3ff8684fb90cc3565c3fe947f297dc03d4a82af
hyezuu <duudu0704@gmail.com>
2025-04-19 03:01:21 +0900

    [#144][review] refactor : 테스트 코드 수정

--- review/src/test/java/table/eat/now/review/application/scheduler/ReviewSchedulerTest.java
+++ review/src/test/java/table/eat/now/review/application/scheduler/ReviewSchedulerTest.java
@@ -1,15 +1,16 @@
 package table.eat.now.review.application.scheduler;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import table.eat.now.review.application.usecase.UpdateRestaurantRatingUseCase;
-
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReviewSchedulerTest {
@@ -25,10 +26,6 @@ class ReviewSchedulerTest {
 
     @Test
     void execute_메서드를_호출할_수_있다() {
-      // given
-      int batchSize = 100;
-      ReflectionTestUtils.setField(reviewScheduler, "batchSize", batchSize);
-
       // when
       reviewScheduler.updateRestaurantRatings();
 
@@ -39,9 +36,6 @@ class ReviewSchedulerTest {
     @Test
     void 예외가_발생해도_정상적으로_동작한다() {
       // given
-      int batchSize = 100;
-      ReflectionTestUtils.setField(reviewScheduler, "batchSize", batchSize);
-
       doThrow(new RuntimeException("Test exception"))
           .when(updateRestaurantRatingUseCase).execute();
 
