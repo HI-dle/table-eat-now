cbb4d7f7eca6badf3dd1f4e94ef581deeb4c2fd4
je-pa <jieun0502@gmail.com>
2025-04-20 22:08:06 +0900

    [#158][reservation] fix: 이벤트에 eventType 추가

--- reservation/src/main/java/table/eat/now/reservation/reservation/application/event/event/ReservationCanceledEvent.java
+++ reservation/src/main/java/table/eat/now/reservation/reservation/application/event/event/ReservationCanceledEvent.java
@@ -3,18 +3,14 @@ package table.eat.now.reservation.reservation.application.event.event;
 import table.eat.now.reservation.reservation.application.event.payload.ReservationCanceledPayload;
 
 public record ReservationCanceledEvent (
+    EventType eventType,
     String reservationUuid,
     ReservationCanceledPayload payload
 
 ) implements ReservationEvent {
 
   public static ReservationCanceledEvent from(ReservationCanceledPayload payload){
-    return new ReservationCanceledEvent(payload.reservationUuid(), payload);
-  }
-
-  @Override
-  public EventType eventType() {
-    return EventType.RESERVATION_CANCELED;
+    return new ReservationCanceledEvent(EventType.RESERVATION_CANCELED, payload.reservationUuid(), payload);
   }
 
 }
