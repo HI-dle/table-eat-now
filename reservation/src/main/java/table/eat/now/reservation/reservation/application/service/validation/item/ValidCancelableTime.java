9c36c61205813ff683dbd3889266fbbacec42d4e
je-pa <jieun0502@gmail.com>
2025-04-21 23:08:58 +0900

    [#168][reservation] refactor: valid 컴포넌트 리펙토링

--- reservation/src/main/java/table/eat/now/reservation/reservation/application/service/validation/item/ValidCancelableTime.java
+++ reservation/src/main/java/table/eat/now/reservation/reservation/application/service/validation/item/ValidCancelableTime.java
@@ -18,17 +18,6 @@ public class ValidCancelableTime implements ValidItem<CancelReservationValidatio
 
   private static final long MINIMUM_HOURS_BEFORE_CANCELLATION = 3;
 
-  @Override
-  public void validate(CancelReservationValidationContext ctx) {
-    validateReservationStatus(ctx.reservation());
-
-    validateCancellationDeadline(
-        ctx.cancelRequestDateTime(), 
-        ctx.reservation().getRestaurantTimeSlotDetails().reservationDateTime());
-
-    validateEditableUser(ctx.reservation(), ctx.requesterId(), ctx.userRole());
-  }
-
   private static void validateReservationStatus(Reservation reservation) {
     if (reservation.isCanceled()) {
       throw CustomException.from(ReservationErrorCode.ALREADY_CANCELED);
@@ -37,8 +26,8 @@ public class ValidCancelableTime implements ValidItem<CancelReservationValidatio
 
   private static void validateCancellationDeadline(LocalDateTime cancelRequestDateTime,
       LocalDateTime reservationTime) {
-    if (Duration.between(cancelRequestDateTime, reservationTime).toHours()
-        < MINIMUM_HOURS_BEFORE_CANCELLATION) {
+    Duration duration = Duration.between(cancelRequestDateTime, reservationTime);
+    if (duration.toMinutes() < MINIMUM_HOURS_BEFORE_CANCELLATION * 60) {
       throw CustomException.from(ReservationErrorCode.CANCELLATION_DEADLINE_PASSED);
     }
   }
@@ -48,5 +37,16 @@ public class ValidCancelableTime implements ValidItem<CancelReservationValidatio
     if (!reservation.isEditableBy(requesterId, requesterUserRole)) {
       throw CustomException.from(ReservationErrorCode.NO_CANCEL_PERMISSION);
     }
+  }
+
+  @Override
+  public void validate(CancelReservationValidationContext ctx) {
+    validateReservationStatus(ctx.reservation());
+
+    validateCancellationDeadline(
+        ctx.cancelRequestDateTime(),
+        ctx.reservation().getRestaurantTimeSlotDetails().reservationDateTime());
+
+    validateEditableUser(ctx.reservation(), ctx.requesterId(), ctx.userRole());
   }
 }
