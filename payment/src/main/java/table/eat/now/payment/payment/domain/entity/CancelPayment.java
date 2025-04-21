fde30e54ed1d1bb3a690fa1993310d0871215963
hyezuu <duudu0704@gmail.com>
2025-04-21 16:16:16 +0900

    [#167][payment] refactor : 추가 수정 및 api 테스트

--- payment/src/main/java/table/eat/now/payment/payment/domain/entity/CancelPayment.java
+++ payment/src/main/java/table/eat/now/payment/payment/domain/entity/CancelPayment.java
@@ -1,10 +1,13 @@
 package table.eat.now.payment.payment.domain.entity;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record CancelPayment(
     String paymentKey,
     String cancelReason,
+    BigDecimal cancelAmount,
+    BigDecimal balanceAmount,
     LocalDateTime cancelledAt
 ) {
 }
