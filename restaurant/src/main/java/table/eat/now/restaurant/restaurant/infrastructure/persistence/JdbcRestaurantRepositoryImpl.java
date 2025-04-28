487cacbd947b1fe5bd6018cefd91df5d3ae53cf1
je-pa <jieun0502@gmail.com>
2025-04-29 01:43:04 +0900

    [#215][restaurant] refactor: jdbc repository batch update rating

--- restaurant/src/main/java/table/eat/now/restaurant/restaurant/infrastructure/persistence/JdbcRestaurantRepositoryImpl.java
+++ restaurant/src/main/java/table/eat/now/restaurant/restaurant/infrastructure/persistence/JdbcRestaurantRepositoryImpl.java
@@ -5,7 +5,6 @@
 package table.eat.now.restaurant.restaurant.infrastructure.persistence;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -18,14 +17,17 @@ public class JdbcRestaurantRepositoryImpl implements JdbcRestaurantRepository {
   private final JdbcTemplate jdbcTemplate;
 
   /**
+   * data라는 가상의 테이블을 만들고 restaurant_uuid가 같은 걸 찾아서 review_rating_avg를 업데이트한다.
    * 예시
-   * UPDATE p_restaurant
-   * SET review_rating_avg = CASE restaurant_uuid
-   * WHEN 'uuid1' THEN 4.5
-   * WHEN 'uuid2' THEN 3.8
-   * WHEN 'uuid3' THEN 5.0
-   * END
-   * WHERE restaurant_uuid IN ('uuid1', 'uuid2', 'uuid3');
+   * UPDATE p_restaurant r
+   * SET review_rating_avg = data.average_rating
+   * FROM (
+   *   VALUES
+   *     ('uuid1', 4.5),
+   *     ('uuid2', 3.8),
+   *     ('uuid3', 5.0)
+   * ) AS data(restaurant_uuid, average_rating)
+   * WHERE r.restaurant_uuid = data.restaurant_uuid;
    */
   @Override
   public void batchModifyRestaurantRating(List<RestaurantRatingUpdatedCommand> commands) {
@@ -34,23 +36,23 @@ public class JdbcRestaurantRepositoryImpl implements JdbcRestaurantRepository {
     }
 
     StringBuilder sql = new StringBuilder();
-    sql.append("UPDATE p_restaurant SET review_rating_avg = CASE restaurant_uuid ");
+    sql.append("UPDATE p_restaurant r SET review_rating_avg = data.average_rating FROM (VALUES ");
 
-    for (RestaurantRatingUpdatedCommand command : commands) {
-      sql.append("WHEN '")
+    for (int i = 0; i < commands.size(); i++) {
+      RestaurantRatingUpdatedCommand command = commands.get(i);
+      sql.append("('")
           .append(command.restaurantUuid())
-          .append("' THEN ")
+          .append("', ")
           .append(command.averageRating())
-          .append(" ");
+          .append(")");
+
+      if (i < commands.size() - 1) {
+        sql.append(", ");
+      }
     }
 
-    sql.append("END WHERE restaurant_uuid IN (");
-    sql.append(
-        commands.stream()
-            .map(command -> "'" + command.restaurantUuid() + "'")
-            .collect(Collectors.joining(", "))
-    );
-    sql.append(")");
+    sql.append(") AS data(restaurant_uuid, average_rating) ");
+    sql.append("WHERE r.restaurant_uuid = data.restaurant_uuid");
 
     jdbcTemplate.update(sql.toString());
   }
