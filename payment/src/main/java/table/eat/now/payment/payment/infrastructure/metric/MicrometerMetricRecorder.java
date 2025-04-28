83e9caa8577a6ecbcbd917e7885a4f942bcec6c4
hyezuu <duudu0704@gmail.com>
2025-04-28 16:36:33 +0900

    [#211][payment] chore : 결제 메트릭설정 추가

---
+++ payment/src/main/java/table/eat/now/payment/payment/infrastructure/metric/MicrometerMetricRecorder.java
@@ -0,0 +1,44 @@
+package table.eat.now.payment.payment.infrastructure.metric;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import table.eat.now.payment.payment.application.metric.MetricRecorder;
+
+@RequiredArgsConstructor
+@Component
+public class MicrometerMetricRecorder implements MetricRecorder {
+
+  private final MeterRegistry registry;
+
+  @Override
+  public void countSuccess(String metricName) {
+    registry.counter(metricName + ".success").increment();
+  }
+
+  @Override
+  public void countFailure(String metricName) {
+    registry.counter(metricName + ".failure").increment();
+  }
+
+  @Override
+  public void recordTime(String metricName, Runnable task) {
+    Timer.Sample sample = Timer.start(registry);
+    try {
+      task.run();
+    } finally {
+      sample.stop(registry.timer(metricName + ".duration"));
+    }
+  }
+
+  @Override
+  public void countSuccess(String metricName, String tagKey, String tagValue) {
+    registry.counter(metricName + ".success", tagKey, tagValue).increment();
+  }
+
+  @Override
+  public void countFailure(String metricName, String tagKey, String tagValue) {
+    registry.counter(metricName + ".failure", tagKey, tagValue).increment();
+  }
+}
