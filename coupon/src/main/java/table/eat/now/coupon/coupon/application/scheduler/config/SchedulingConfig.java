2065e052c94d64807f41c48f954e5b890eb0de7c
HanaHww2 <hanahww2@gmail.com>
2025-05-09 13:59:54 +0900

    [#264][coupon] refactor: 스케쥴러 스레드풀 설정

--- coupon/src/main/java/table/eat/now/coupon/coupon/application/scheduler/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package table.eat.now.coupon.coupon.application.scheduler.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {
-}
